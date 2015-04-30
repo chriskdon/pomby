051e4fad768bd504bd0385994c120a82bee5110d
Mike Valstar <mvalstar@konradgroup.com>
2015-04-30 16:52:30 -0400

    Selectable tabs for the 2 types of login

--- javascript/UI/login/tabs.jsx
+++ javascript/UI/login/tabs.jsx
@@ -5,8 +5,25 @@ if (typeof(UI.login) === 'undefined') UI.login = {};
 
 UI.login.tabs = React.createClass({
 
+    handleTab1Click: function(e) {
+        $(e.target).addClass('active');
+        $(this.refs.tab2.getDOMNode()).removeClass('active');
+    },
+
+    handleTab2Click: function(e) {
+        $(e.target).addClass('active');
+        $(this.refs.tab1.getDOMNode()).removeClass('active');
+    },
+
     render: function() {
-        return <div className="col-xs-6">Login Tabs</div>
+        return (
+            <div className="col-xs-6">
+                <div className="row">
+                    <div className="col-xs-6 login-tab active" ref="tab1" onClick={this.handleTab1Click}>Employee Login</div>
+                    <div className="col-xs-6 login-tab" ref="tab2" onClick={this.handleTab2Click}>Client Login</div>
+                </div>
+            </div>
+        )
     }
 
 });
